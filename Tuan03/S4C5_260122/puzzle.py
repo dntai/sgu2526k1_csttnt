a1f2addccee534fc88b21f5ac7fe5d5d433c495b
Tai Do <donhutai@gmail.com>
2026-01-22 14:02:46 +0700

    Tuan03\Taci\Minh hoa

--- Tuan03/S4C5_260122/puzzle.py
+++ Tuan03/S4C5_260122/puzzle.py
@@ -1,8 +1,34 @@
 FI = "puzzle.inp"
 
 class State:
-    def __init__(self):
+    actions = [(-1,0),(1,0),(0,-1),(0,1)]
+    
+    def __init__(self, key, parent = None, cost = 0):
+        self.key = [[v for v in row] for row in key]
+        self.pos0 = State.find0(self.key)
+        self.parent = parent
+        self.cost = cost
         pass
+
+    @staticmethod
+    def tokey(key):
+        return tuple([tuple([v for v in row]) for row in key])
+        pass
+
+    def tokey(self):
+        return State.tokey(self.key)
+        pass
+
+    @staticmethod
+    def find0(key):
+        for i in range(3):
+            for j in range(3):
+                if key[i][j] == 0:
+                    return (i,j)
+        return (-1,-1)
+        pass
+    
+    
     pass
 
 def solve(**kwargs):
@@ -21,6 +47,8 @@ def solve(**kwargs):
         [int(v.replace('\n', '')) for v in row.split(' ')] 
         for row in content[3:6]
     ]
+
+    print(State(start).tokey())
     
     kwargs.get('debug', {}).update(**locals())
     pass # solve
