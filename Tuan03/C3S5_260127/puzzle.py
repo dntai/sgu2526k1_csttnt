0cb294092560b2a5d0f588b78d12fd66d4191413
Tai Do <donhutai@gmail.com>
2026-01-27 14:09:40 +0700

    Tuan03/Bai 8Puzzle/Lam bai

--- Tuan03/C3S5_260127/puzzle.py
+++ Tuan03/C3S5_260127/puzzle.py
@@ -2,14 +2,40 @@
 
 FI = "puzzle.inp"
 
-def DocFile(fpath = FI, **kwargs):
-    with open(fpath, 'rt') as file:
-        content = file.readlines()
-    kwargs.get('debug',{}).update(locals())
+class State:
+    def __init__(self, key, parent = None, cost = 0):
+        self.key = [[c for c in rows] for rows in key]
+        self.parent = parent
+        self.cost = cost
+        pass
+
+    def convertKey(key):
+        return tuple([tuple([c for c in rows]) for rows in key])
+        pass
+
+    def tokey(self):
+        return State.convertKey(self.key)
+        pass
+    
     pass
 
-def test1():
-    DocFile(debug = globals())
+def test1(fpath = FI, **kwargs):
+    with open(fpath, 'rt') as file:
+        content = file.readlines()
+
+    start = [
+        [int(ci.replace('\n','')) for ci in row.split(' ')] 
+        for row in content[:3]
+    ]
+
+    goal = [
+        [int(ci.replace('\n','')) for ci in row.split(' ')] 
+        for row in content[3:]
+    ]
+
+    print('Start: ', State(start).tokey())
+    print('Goal: ', State(goal).tokey())
+        
     kwargs.get('debug',{}).update(locals())
     pass
 
